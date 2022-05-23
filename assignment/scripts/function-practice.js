68e2e6c4c2b654c96accf94bfd646e0de9159b2b
Stephon Buckhanan <buckhananstephon@gmail.com>
2022-05-23 18:59:40 -0500

    remove unneeded array

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -1,4 +1,4 @@
-console.log('***** Function Practice *****')
+console.log('***** Function Practice *****');
 
 // Add the required code to complete the functions below
 // After _each_ function, use a console log to call the function
@@ -6,7 +6,7 @@ console.log('***** Function Practice *****')
 
 // 1. Function to return 'Hello World!'
 function hello() {
-  return 'Hello World!';
+	return 'Hello World!';
 }
 // Call the function to test
 console.log('Test - should say "Hello World!"', hello());
@@ -14,116 +14,106 @@ console.log('It is a great day,', hello());
 
 // 2. Function to return an personalized hello, using the `name` argument.
 //    for example 'Hello, Jo!', or 'Hello, Stacy!'
-function helloName( name ) {
-  return console.log('Hello', name);
+function helloName(name) {
+	return console.log('Hello', name);
 }
 // Remember to call the function to test
 helloName('stephon');
 
 // 3. Function to add two numbers together & return the result
-function addNumbers( firstNumber, secondNumber ) {
-  let sum = firstNumber + secondNumber;
-  return sum;
-  // return firstNumber + secondNumber;
+function addNumbers(firstNumber, secondNumber) {
+	let sum = firstNumber + secondNumber;
+	return sum;
+	// return firstNumber + secondNumber;
 }
-console.log( 'Addition of 10 and 10:', addNumbers(10, 10) );
+console.log('Addition of 10 and 10:', addNumbers(10, 10));
 
 // 4. Function to multiply three numbers & return the result
-function multiplyThree( firstNumber, secondNumber, thirdNumber ) {
-  let sum = firstNumber * secondNumber * thirdNumber;
-  return sum;
+function multiplyThree(firstNumber, secondNumber, thirdNumber) {
+	let sum = firstNumber * secondNumber * thirdNumber;
+	return sum;
 }
-console.log( 'Mutiple of 3,3,3:', multiplyThree(3, 3, 3) );
+console.log('Mutiple of 3,3,3:', multiplyThree(3, 3, 3));
 
-// 5. Function that will return true if a number is positive, 
+// 5. Function that will return true if a number is positive,
 //    or greater than zero, and false otherwise
-function isPositive( number ) {
-  if ( number > 0 ){
-    return true;
-  }
-  else{
-    return false;
-  }
+function isPositive(number) {
+	if (number > 0) {
+		return true;
+	} else {
+		return false;
+	}
 }
-// Call the function to test each outcome (true & false) 
+// Call the function to test each outcome (true & false)
 // Write a separate console.log statement for each outcome
-console.log( 'isPositive - should say true:', isPositive(3) );
-console.log( 'isPositive - should say false:', isPositive(0) );
-console.log( 'isPositive - should say false:', isPositive(-3) );
-console.log( 'Should say true:', isPositive(10) );
-console.log( 'Should say false:', isPositive(-6) );
+console.log('isPositive - should say true:', isPositive(3));
+console.log('isPositive - should say false:', isPositive(0));
+console.log('isPositive - should say false:', isPositive(-3));
+console.log('Should say true:', isPositive(10));
+console.log('Should say false:', isPositive(-6));
 
-
-// 6. Function to return the _last_ item in an array. If the 
+// 6. Function to return the _last_ item in an array. If the
 //    array is empty, return `undefined`.
-function getLast( array ) {
-  if (array.length > 0) {
-    return array[array.length - 1];
-  }
-  else{
-    return 'undefined';
-  }
+function getLast(array) {
+	if (array.length > 0) {
+		return array[array.length - 1];
+	} else {
+		return 'undefined';
+	}
 }
 let testArray = ['Drey', 'Deon', 'Diesel', 'Diangelo'];
-let emptyArray = [];
 console.log('This should return last name:', getLast(testArray));
 
-// 7. Function to find a value in an array. Return true if the 
+// 7. Function to find a value in an array. Return true if the
 //    value is found and false otherwise. Use a loop;
-//    DO NOT use Array.includes, Array.indexOf, or Array.find 
+//    DO NOT use Array.includes, Array.indexOf, or Array.find
 
-function find( value, array ){
-  let i = 0;
-  while (i < array.length) {
-    i++;
-    if (array[i] === value) {
-      return true;
-    }
-    else{
-      return false;
-    }
-  }
+function find(value, array) {
+	let i = 0;
+	while (i < array.length) {
+		i++;
+		if (array[i] === value) {
+			return true;
+		} else {
+			return false;
+		}
+	}
 }
 
 console.log('Should return true:', find('Deon', testArray));
 console.log('Should return false:', find('Bro', testArray));
 
-
 // ----------------------
 // Stretch Goals
 // ----------------------
-// 8. Function to check if a letter is the first letter in a 
+// 8. Function to check if a letter is the first letter in a
 //    string. Return true if it is, and false otherwise
 function isFirstLetter(letter, string) {
-  if(letter === string[0]) {
-    return true;
-  }
-  else{
-    return false;
-  }
+	if (letter === string[0]) {
+		return true;
+	} else {
+		return false;
+	}
 }
-console.log( 'isFirstLetter - should say true:', isFirstLetter('a', 'apple') );
-console.log( 'isFirstLetter - should say false:', isFirstLetter('z', 'apple') );
+console.log('isFirstLetter - should say true:', isFirstLetter('a', 'apple'));
+console.log('isFirstLetter - should say false:', isFirstLetter('z', 'apple'));
 
 // 9. Function to return the sum of all numbers in an array
 let numbersArray = [15, 33, -10, 5];
-function sumAll( array ) {
-  let sum = 0
-  for (let i = 0; i < array.length; i++) {
-    sum += array[i];
-  }
-  return sum;
+function sumAll(array) {
+	let sum = 0;
+	for (let i = 0; i < array.length; i++) {
+		sum += array[i];
+	}
+	return sum;
 }
 
 console.log('The sum of the numbers is:', sumAll(numbersArray));
-
 
 // 10. Function to return a new array of all positive (greater than zero)
 //     numbers contained in an input array. If there are no positive numbers
 //     return an empty array. Note: The input array should not change.
 
-
-
-// 11. Pick a problem from Edabit(https://edabit.com/) or 
-//     CodeWars(https://www.codewars.com/). Then describe it 
+// 11. Pick a problem from Edabit(https://edabit.com/) or
+//     CodeWars(https://www.codewars.com/). Then describe it
 //     here in a comment, write the function, and test it!
